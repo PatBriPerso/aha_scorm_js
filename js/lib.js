2bdcb7714e23c96e0a3ed58f5b35159cd064242c
Timothy Johnson <timothy@wearefound.com>
2013-06-04 09:16:48 -0400

    trackng down flow bugs

--- js/lib.js
+++ js/lib.js
@@ -1,9 +1,7 @@
+alert("i cant believe this")
 document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js';
-var getScorm = null
 function getBaseScorm() { 
-  alert("Getting Base Scorm")
   document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='//ahajs.herokuapp.com/js/base_scorm.js';
-  getScorm = null;
 }
 var getScorm = setTimeout("getBaseScorm()", 1000);
 
