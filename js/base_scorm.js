41c8634e9da20f88efec913427f19f7da6e94361
Timothy Johnson <timothy@wearefound.com>
2013-06-04 09:33:59 -0400

    trackng down flow bugs

--- js/base_scorm.js
+++ js/base_scorm.js
@@ -37,7 +37,7 @@ function setupScoreFrame(){
 
 function getScores()
 {
-		var getScoresUrl = scormSrvUrl + "?func=get_scores&orgID="+orgId+"&orgKey="+orgKey+"&courseID="+courseId+"&extID="+userId+"&returnUrl="+ReturnUrl;
+		var getScoresUrl = scormSrvUrl + "?func=get_scores&orgID="+orgId+"&orgKey="+orgKey+"&courseID="+courseId+"&extID="+userId;
     console.log("getScores(); url = "+getScoresUrl)
     $("#scoreCheckFrame").html('<iframe name="scoreFrame" src="' + getScoresUrl + '"></iframe>');
     // reportTimer = setTimeout(reportTheScore, 2000);
