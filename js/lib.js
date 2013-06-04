badb6fe7d3e8818f3db8e624236799a196d17aea
Timothy Johnson <timothy@wearefound.com>
2013-06-04 07:40:58 -0400

    removed js in root

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,7 @@
 document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js';
-document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='//ahajs.herokuapp.com/js/base_scorm.js';
+
+function getBaseScorm() { 
+  document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='//raw.github.com/phoenix-scitent/aha_scorm_js/master/base_scorm.js';
+}
+setTimeout("getBaseScorm()", 1000);
 
--- lib.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js';
-
-function getBaseScorm() { 
-  document.getElementsByTagName('head')[0].appendChild(document.createElement('script')).src='//raw.github.com/phoenix-scitent/aha_scorm_js/master/base_scorm.js';
-}
-setTimeout("getBaseScorm()", 1000);
-
